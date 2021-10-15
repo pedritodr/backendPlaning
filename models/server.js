6c42591b2b08ea1c01793dc397dc1f7692fd7f10
Pedrito <pedroduran014@gmail.com>
2021-10-15 11:39:54 -0500

    correcciones

--- models/server.js
+++ models/server.js
@@ -29,7 +29,7 @@ class Server {
     }
 
     cronJobs() {
-        cron.schedule('* 1 * * *', () => {
+        cron.schedule('* */58 * * *', () => {
             console.log('running a task every hours');
             createToFileFtp();
         });
